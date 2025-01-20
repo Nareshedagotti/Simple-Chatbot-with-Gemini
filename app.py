0d6538f15d6ffc0a0dab21d597cbabfce7130183
Nareshedagotti <naresh21375019@gmail.com>
2025-01-20 18:39:58 +0530

    simple gemini chatbot with GeminiAI

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import streamlit as st
 load_dotenv()
 
-GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY=st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=GEMINI_API_KEY)
 
 def build_prompt(user_input):
